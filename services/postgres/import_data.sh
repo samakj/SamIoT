f2b2d95d803f589c3d8a6f902346a002e3dd3597
Sam Jones <samakj@live.co.uk>
2021-11-28 17:24:44 +0000

    fix: Add data export script

--- services/postgres/import_data.sh
+++ services/postgres/import_data.sh
@@ -1,21 +1,24 @@
 source ./env.sh
 source ./init_roles.sh
 
+services="$(dirname "$(pwd)")"
+root="$(dirname $services)"
+
 export PGPASSWORD=$IOT_PASS
-if [ -f "../../data/iot/devices.csv" ]; then
+if [ -f "$root/data/iot/devices.csv" ]; then
     echo "\nImporting devices data...\n"
-    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --command="COPY devices FROM '../../data/iot/devices.csv' DELIMITER ',' CSV HEADER"
+    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --command="COPY devices FROM '$root/data/iot/devices.csv' DELIMITER ',' CSV HEADER"
 fi
-if [ -f "../../data/iot/locations.csv" ]; then
+if [ -f "$root/data/iot/locations.csv" ]; then
     echo "\nImporting locations data...\n"
-    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --command="COPY locations FROM '../../data/iot/locations.csv' DELIMITER ',' CSV HEADER"
+    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --command="COPY locations FROM '$root/data/iot/locations.csv' DELIMITER ',' CSV HEADER"
 fi
-if [ -f "../../data/iot/metrics.csv" ]; then
+if [ -f "$root/data/iot/metrics.csv" ]; then
     echo "\nImporting metrics data...\n"
-    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --command="COPY metrics FROM '../../data/iot/metrics.csv' DELIMITER ',' CSV HEADER"
+    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --command="COPY metrics FROM '$root/data/iot/metrics.csv' DELIMITER ',' CSV HEADER"
 fi
-if [ -f "../../data/iot/measurements.csv" ]; then
+if [ -f "$root/data/iot/measurements.csv" ]; then
     echo "\nImporting measurements data...\n"
-    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --command="COPY measurements FROM '../../data/iot/measurements.csv' DELIMITER ',' CSV HEADER"
+    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --command="COPY measurements FROM '$root/data/iot/measurements.csv' DELIMITER ',' CSV HEADER"
 fi
 export PGPASSWORD=
