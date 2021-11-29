ed2d0689a6e730b778e7176ddc0919e59e4cdb48
Sam Jones <samakj@live.co.uk>
2021-11-29 14:38:09 +0000

    fix: Fix invalid foreign key types

--- shared/python/models/Device.py
+++ shared/python/models/Device.py
@@ -2,13 +2,11 @@ from datetime import datetime
 from typing import Optional
 from pydantic import BaseModel, IPvAnyAddress
 
-from shared.python.models.Location import Location
-
 
 class Device(BaseModel):
     id: int
     mac: str
     ip: IPvAnyAddress
     websocket_path: str
-    location_id: Location.id
+    location_id: int
     last_message: Optional[datetime] = None
--- shared/python/models/Measurement.py
+++ shared/python/models/Measurement.py
@@ -3,10 +3,6 @@ from decimal import Decimal
 from typing import List, Literal, Optional, Union
 from pydantic import BaseModel
 
-from shared.python.models.Device import Device
-from shared.python.models.Location import Location
-from shared.python.models.Metric import Metric
-
 ValueTypeOptions = Literal["string", "float", "integer", "boolean"]
 ValueType = Union[int, str, Decimal, bool, None]
 
@@ -14,9 +10,9 @@ ValueType = Union[int, str, Decimal, bool, None]
 class Measurement(BaseModel):
     id: int
     timestamp: datetime
-    device_id: Device.id
-    location_id: Location.id
-    metric_id: Metric.id
+    device_id: int
+    location_id: int
+    metric_id: int
     tags: Optional[List[str]] = None
     value_type: ValueTypeOptions = "string"
     value: ValueType = None
