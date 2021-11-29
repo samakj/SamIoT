b8faeb22c65e567ed763376886a2a8b4decece2f
Sam Jones <samakj@live.co.uk>
2021-11-29 15:13:38 +0000

    fix: Run api when starting dev script

--- services/python/iot/api/run_dev_env.sh
+++ services/python/iot/api/run_dev_env.sh
@@ -10,3 +10,11 @@ source bin/activate
 ln -s "$root/shared" ./
 
 pip3 install -r requirements.txt
+
+gunicorn app:create_app \
+    --bind 0.0.0.0:8080 \
+    --worker-class aiohttp.GunicornWebWorker \
+    --log-level 'info' \
+    --reload
+
+deactivate
