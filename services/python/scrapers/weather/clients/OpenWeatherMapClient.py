0ce71520d37c75ae1aaab1a4486020d151eb44d8
Sam Jones <samakj@live.co.uk>
2021-12-03 21:24:38 +0000

    fix: Add OpenWeatherMap client for fetching weather data

---
+++ services/python/scrapers/weather/clients/OpenWeatherMapClient.py
@@ -0,0 +1,46 @@
+from decimal import Decimal
+from datetime import datetime
+
+from models.OpenWeatherMap import OpenWeatherMapCurrentAndForecastResponse, OpenWeatherMapHistoricResponse
+from shared.python.extensions.aiohttp.api_client import APIClient
+
+
+class OpenWeatherMapClient(APIClient):
+    api_key: str
+
+    def __init__(self, api_key: str) -> None:
+        super().__init__("api.openweathermap.org/data/2.5/", 443, ssl=True)
+        self.api_key = api_key
+
+    async def get_current_and_forecast(
+        self, lat: Decimal, lon: Decimal
+    ) -> OpenWeatherMapCurrentAndForecastResponse:
+        response = await self.get(
+            "/onecall",
+            params={
+                "lat": lat,
+                "lon": lon,
+                "units": "metric",
+                "appid": self.api_key,
+            }
+        )
+        return OpenWeatherMapCurrentAndForecastResponse.parse_obj(
+            await response.json()
+        )
+
+    async def get_historic(
+        self, lat: Decimal, lon: Decimal, date: datetime
+    ) -> OpenWeatherMapHistoricResponse:
+        response = await self.get(
+            "/onecall/timemachine",
+            params={
+                "lat": lat,
+                "lon": lon,
+                "date": datetime.fromisoformat(date.isoformat()[:10]).timestamp(),
+                "units": "metric",
+                "appid": self.api_key,
+            }
+        )
+        return OpenWeatherMapHistoricResponse.parse_obj(
+            await response.json()
+        )
---
+++ services/python/scrapers/weather/models/OpenWeatherMap.py
@@ -0,0 +1,109 @@
+from datetime import datetime
+from decimal import Decimal
+from pydantic import BaseModel
+from typing import List, Optional
+
+
+class OpenWeatherMapWeatherResponse(BaseModel):
+    id: int
+    main: str
+    description: str
+    icon: str
+
+
+class OpenWeatherMapCurrentResponse(BaseModel):
+    dt: datetime
+    sunrise: datetime
+    sunset: datetime
+    temp: Decimal
+    feels_like: Decimal
+    pressure: int
+    humidity: int
+    dew_point: Decimal
+    clouds: int
+    visibility: int
+    wind_speed: Decimal
+    wind_deg: int
+    weather: List[OpenWeatherMapWeatherResponse]
+    wind_gust: Optional[Decimal] = None
+    uvi: Optional[Decimal] = None
+
+
+class OpenWeatherMapMinutelyResponse(BaseModel):
+    dt: int
+    precipitation: Decimal
+
+
+class OpenWeatherMapHourlyResponse(BaseModel):
+    dt: datetime
+    temp: Decimal
+    feels_like: Decimal
+    pressure: int
+    humidity: int
+    dew_point: Decimal
+    clouds: int
+    wind_speed: Decimal
+    wind_deg: int
+    weather: List[OpenWeatherMapWeatherResponse]
+    pop: Decimal
+    wind_gust: Optional[Decimal] = None
+    uvi: Optional[Decimal] = None
+    visibility: Optional[int] = None
+
+
+class OpenWeatherMapDailyTempResponse(BaseModel):
+    day: Decimal
+    min: Decimal
+    max: Decimal
+    night: Decimal
+    eve: Decimal
+    morn: Decimal
+
+
+class OpenWeatherMapDailyFeelsLikeResponse(BaseModel):
+    day: Decimal
+    night: Decimal
+    eve: Decimal
+    morn: Decimal
+
+
+class OpenWeatherMapDailyResponse(BaseModel):
+    dt: datetime
+    sunrise: datetime
+    sunset: datetime
+    moonrise: datetime
+    moonset: datetime
+    moon_phase: datetime
+    temp: OpenWeatherMapDailyTempResponse
+    feels_like: OpenWeatherMapDailyFeelsLikeResponse
+    pressure: int
+    humidity: int
+    dew_point: Decimal
+    uvi: Decimal
+    rain: Decimal
+    pop: Decimal
+    clouds: int
+    wind_speed: Decimal
+    wind_deg: int
+    weather: List[OpenWeatherMapWeatherResponse]
+    wind_gust: Optional[Decimal] = None
+
+
+class OpenWeatherMapCurrentAndForecastResponse(BaseModel):
+    lat: Decimal
+    lon: Decimal
+    timezone: str
+    timezone_offset: int
+    current: OpenWeatherMapCurrentResponse
+    minutely: List[OpenWeatherMapMinutelyResponse]
+    hourly: List[OpenWeatherMapHourlyResponse]
+    daily: List[OpenWeatherMapDailyResponse]
+
+
+class OpenWeatherMapHistoricResponse(BaseModel):
+    lat: Decimal
+    lon: Decimal
+    timezone: str
+    timezone_offset: int
+    current: OpenWeatherMapCurrentResponse
+    hourly: List[OpenWeatherMapHourlyResponse]
