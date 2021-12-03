50044cfc4264c87f1963c898f29edbac5fa341e3
Sam Jones <samakj@live.co.uk>
2021-12-03 09:25:42 +0000

    fix: Add store for handling websocket connections

--- services/python/iot/forager/Application.py
+++ services/python/iot/forager/Application.py
@@ -1,11 +1,21 @@
 import os
 from aiohttp.web import Application
 
+from stores.device_websockets import DeviceWebsocketsStore
 from shared.python.clients.iot import IoTClient
+from shared.python.queue import Queue
 
 
 class ForagerApplication(Application):
     iot_client: IoTClient
+    queue: Queue
+    device_websocket_store: DeviceWebsocketsStore
+
+    def connect_queue(self) -> None:
+        self.queue = Queue()
+
+    def connect_device_websocket_store(self) -> None:
+        self.device_websocket_store = DeviceWebsocketsStore()
 
     def connect_iot_client(self) -> None:
         self.iot_client = IoTClient(
---
+++ services/python/iot/forager/stores/device_websockets.py
@@ -0,0 +1,83 @@
+import logging
+from typing import Dict, Optional
+from datetime import datetime
+
+from aiohttp.http_websocket import WSMessage
+
+from shared.python.extensions.aiohttp.websocket import EventCallback, EventWithMessageCallback, Websocket
+from shared.python.clients.iot import IoTClient
+from shared.python.models.Device import Device
+
+LOG = logging.getLogger()
+
+
+class DeviceWebsocketsStore:
+    websockets: Dict[int, Websocket]
+
+    def __init__(self) -> None:
+        self.websockets = {}
+
+    async def create(
+        self,
+        device: Device,
+        *,
+        on_connect: Optional[EventCallback],
+        on_disconnect: Optional[EventCallback],
+        on_message: Optional[EventWithMessageCallback],
+        on_text: Optional[EventWithMessageCallback],
+        on_json: Optional[EventWithMessageCallback],
+        on_error: Optional[EventWithMessageCallback],
+        on_close: Optional[EventWithMessageCallback],
+    ) -> Optional[Websocket]:
+        if device.id in self.websockets:
+            LOG.warning(
+                f"Creating {device.id} connected device websocket which exists,"
+                " cancelling old task..."
+            )
+            await self.delete_websocket(device.id)
+
+        self.websockets[device.id] = Websocket(
+            f"ws://{device.ip}{device.websocket_path}",
+            id=f"forager:device:{device.id}",
+            name=f"Device {device.id}",
+            on_connect=on_connect,
+            on_disconnect=on_disconnect,
+            on_message=on_message,
+            on_text=on_text,
+            on_json=on_json,
+            on_error=on_error,
+            on_close=on_close,
+        )
+        await self.websockets[device.id].start()
+
+        return self.websockets[device.id]
+
+    async def get_websocket(self, id: int) -> Optional[Websocket]:
+        return self.websockets.get(id, None)
+
+    async def get_websocket_by_device(
+        self, device: Device
+    ) -> Optional[Websocket]:
+        return await self.get_websocket(device.id)
+
+    async def get_websockets(self) -> Dict[int, Websocket]:
+        return self.websockets
+
+    async def delete_websocket(self, id: int) -> None:
+        LOG.info(f"Shutting down device {id} websocket...")
+        await self.websockets[id].stop()
+        del self.websockets[id]
+        LOG.info(f"Done.")
+
+    async def delete_websocket_by_device(self, device: Device) -> None:
+        return await self.delete_websocket(device.id)
+
+    async def delete_websockets(self) -> None:
+        LOG.info(
+            f"Shutting down {len(self.websockets.keys())} device"
+            " websockets..."
+        )
+        for id in self.websockets:
+            await self.delete_websocket(id)
+
+        LOG.info(f"Done.")
