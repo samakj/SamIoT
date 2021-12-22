20fd8ce65274160099fff099d788a1458a68dae1
Sam Jones <samakj@live.co.uk>
2021-12-22 18:42:39 +0000

    fix: Add ssr support for styled components

--- services/javascript/frontend/server/server.tsx
+++ services/javascript/frontend/server/server.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { App } from 'client/App';
 import { StaticRouter } from 'react-router-dom';
+import { ServerStyleSheet, StyleSheetManager } from 'styled-components';
 
 const server = express();
 
@@ -19,9 +20,16 @@ const assets = JSON.parse(manifest);
 
 server.get('*', (request, response) => {
   const context: { [key: string]: string } = {};
-  const component = ReactDOMServer.renderToString(<App url={request.url} ssr />);
+  const stylesheet = new ServerStyleSheet();
+  const component = ReactDOMServer.renderToString(
+    <StyleSheetManager sheet={stylesheet.instance}>
+      <App url={request.url} ssr />
+    </StyleSheetManager>
+  );
+  const style = stylesheet.getStyleTags();
+  stylesheet.seal();
   if (context.url) response.redirect(301, context.url);
-  response.render('index', { assets, component });
+  response.render('index', { assets, component, style });
 });
 
 server.listen(process.env.FRONTEND_PORT, () => {
