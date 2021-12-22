93fcfa65eb465faf6b7857b3e131499612f82785
Sam Jones <samakj@live.co.uk>
2021-12-22 16:22:06 +0000

    fix: Add redux store to app

--- services/javascript/frontend/client/App.tsx
+++ services/javascript/frontend/client/App.tsx
@@ -1,6 +1,8 @@
 /** @format */
 
 import React from 'react';
+import { store } from 'client/store';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import { Card, FullWidthCard, SmallCard, SmallWideCard } from './components/Cards';
 import { ContentGrid } from './components/ContentGrid';
@@ -9,38 +11,40 @@ import { DarkTheme, GlobalStyle, LightTheme } from './style';
 
 export const App: React.FunctionComponent = () => (
   <>
-    <ThemeProvider theme={DarkTheme}>
-      <GlobalStyle />
-      <ContentGrid>
-        <FullWidthCard>
-          <H2>Header Card</H2>
-          <p></p>
-        </FullWidthCard>
-        <Card>
-          <H2>Normal Card</H2>
-          <p>This is a test card with some content.</p>
-        </Card>
-        <Card>
-          <H2>Normal Card</H2>
-          <p>This is a test card with some content.</p>
-        </Card>
-        <SmallCard>
-          <H2>Small Card</H2>
-          <p>This is a test card with some content.</p>
-        </SmallCard>
-        <SmallCard>
-          <H2>Small Card</H2>
-          <p>This is a test card with some content.</p>
-        </SmallCard>
-        <Card>
-          <H2>Normal Card</H2>
-          <p>This is a test card with some content.</p>
-        </Card>
-        <SmallWideCard>
-          <H2>Small Wide Card</H2>
-          <p>This is a test card with some content.</p>
-        </SmallWideCard>
-      </ContentGrid>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={DarkTheme}>
+        <GlobalStyle />
+        <ContentGrid>
+          <FullWidthCard>
+            <H2>Header Card</H2>
+            <p></p>
+          </FullWidthCard>
+          <Card>
+            <H2>Normal Card</H2>
+            <p>This is a test card with some content.</p>
+          </Card>
+          <Card>
+            <H2>Normal Card</H2>
+            <p>This is a test card with some content.</p>
+          </Card>
+          <SmallCard>
+            <H2>Small Card</H2>
+            <p>This is a test card with some content.</p>
+          </SmallCard>
+          <SmallCard>
+            <H2>Small Card</H2>
+            <p>This is a test card with some content.</p>
+          </SmallCard>
+          <Card>
+            <H2>Normal Card</H2>
+            <p>This is a test card with some content.</p>
+          </Card>
+          <SmallWideCard>
+            <H2>Small Wide Card</H2>
+            <p>This is a test card with some content.</p>
+          </SmallWideCard>
+        </ContentGrid>
+      </ThemeProvider>
+    </Provider>
   </>
 );
