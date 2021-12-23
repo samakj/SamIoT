053540f738f6ff856fb55799e8440cb0791b21f0
Sam Jones <samakj@live.co.uk>
2021-12-23 09:46:15 +0000

    fix: Add light axios wrapper for later extension

---
+++ shared/javascript/extensions/axios/api-client/index.ts
@@ -0,0 +1,54 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from "axios";
+
+export class APIClient {
+  host: string;
+  port?: number | string;
+  ssl: boolean;
+  httpBaseUrl: string;
+  websocketBaseUrl: string;
+  axiosInstance: AxiosInstance;
+
+  constructor(host: string, port: number | string | null, ssl: boolean) {
+    this.host = host;
+    this.port = port;
+    this.ssl = ssl;
+    this.httpBaseUrl =
+      `http${this.ssl ? "s" : ""}://` +
+      `${this.host}${this.port != null ? `:${this.port}` : ""}`;
+    this.websocketBaseUrl =
+      `ws${this.ssl ? "s" : ""}://` +
+      `${this.host}${this.port != null ? `:${this.port}` : ""}`;
+    this.axiosInstance = axios.create({
+      baseURL: this.httpBaseUrl,
+      timeout: 1000,
+    });
+  }
+
+  get<T = any, R = AxiosResponse<T>, D = any>(
+    endpoint: string,
+    config: AxiosRequestConfig<D>
+  ): Promise<R> {
+    return axios.get(endpoint, config);
+  }
+
+  post<T = any, R = AxiosResponse<T>, D = any>(
+    endpoint: string,
+    config: AxiosRequestConfig<D>
+  ): Promise<R> {
+    return axios.post(endpoint, config);
+  }
+
+  patch<T = any, R = AxiosResponse<T>, D = any>(
+    endpoint: string,
+    config: AxiosRequestConfig<D>
+  ): Promise<R> {
+    return axios.patch(endpoint, config);
+  }
+
+  delete<T = any, R = AxiosResponse<T>, D = any>(
+    endpoint: string,
+    config: AxiosRequestConfig<D>
+  ): Promise<R> {
+    return axios.delete(endpoint, config);
+  }
+}
