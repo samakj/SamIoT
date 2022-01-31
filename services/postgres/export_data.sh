cd5d75a4ab69f0cf1a2e4c3f251f6e1e9ebf541e
samakj <samakj@live.co.uk>
2022-01-31 16:18:25 +0000

    fix: Use client side copy for import/export

--- services/postgres/export_data.sh
+++ services/postgres/export_data.sh
@@ -10,56 +10,56 @@ if [ -f "$root/data/iot/devices.csv" ]; then
     mv $root/data/iot/devices.csv $root/data/iot/devices.old.csv
 fi
 echo "\nExporting devices...\n"
-psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="COPY devices TO '$root/data/iot/devices.csv' DELIMITER ',' CSV HEADER;"
+psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="\copy devices TO '$root/data/iot/devices.csv' DELIMITER ',' CSV HEADER;"
 
 if [ -f "$root/data/iot/locations.csv" ]; then
     echo "\nRenaming old locations CSV...\n"
     mv $root/data/iot/locations.csv $root/data/iot/locations.old.csv
 fi
 echo "\nExporting locations...\n"
-psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="COPY locations TO '$root/data/iot/locations.csv' DELIMITER ',' CSV HEADER;"
+psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="\copy locations TO '$root/data/iot/locations.csv' DELIMITER ',' CSV HEADER;"
 
 if [ -f "$root/data/iot/metrics.csv" ]; then
     echo "\nRenaming old metrics CSV...\n"
     mv $root/data/iot/metrics.csv $root/data/iot/metrics.old.csv
 fi
 echo "\nExporting metrics...\n"
-psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="COPY metrics TO '$root/data/iot/metrics.csv' DELIMITER ',' CSV HEADER;"
+psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="\copy metrics TO '$root/data/iot/metrics.csv' DELIMITER ',' CSV HEADER;"
 
 if [ -f "$root/data/iot/measurements.csv" ]; then
     echo "\nRenaming old measurements CSV...\n"
     mv $root/data/iot/measurements.csv $root/data/iot/measurements.old.csv
 fi
 echo "\nExporting measurements...\n"
-psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="COPY measurements TO '$root/data/iot/measurements.csv' DELIMITER ',' CSV HEADER;"
+psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="\copy measurements TO '$root/data/iot/measurements.csv' DELIMITER ',' CSV HEADER;"
 
 if [ -f "$root/data/iot/float_measurements.csv" ]; then
     echo "\nRenaming old float_measurements CSV...\n"
     mv $root/data/iot/float_measurements.csv $root/data/iot/float_measurements.old.csv
 fi
 echo "\nExporting float_measurements...\n"
-psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="COPY float_measurements TO '$root/data/iot/float_measurements.csv' DELIMITER ',' CSV HEADER;"
+psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="\copy float_measurements TO '$root/data/iot/float_measurements.csv' DELIMITER ',' CSV HEADER;"
 
 if [ -f "$root/data/iot/integer_measurements.csv" ]; then
     echo "\nRenaming old integer_measurements CSV...\n"
     mv $root/data/iot/integer_measurements.csv $root/data/iot/integer_measurements.old.csv
 fi
 echo "\nExporting integer_measurements...\n"
-psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="COPY integer_measurements TO '$root/data/iot/integer_measurements.csv' DELIMITER ',' CSV HEADER;"
+psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="\copy integer_measurements TO '$root/data/iot/integer_measurements.csv' DELIMITER ',' CSV HEADER;"
 
 if [ -f "$root/data/iot/string_measurements.csv" ]; then
     echo "\nRenaming old string_measurements CSV...\n"
     mv $root/data/iot/string_measurements.csv $root/data/iot/string_measurements.old.csv
 fi
 echo "\nExporting string_measurements...\n"
-psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="COPY string_measurements TO '$root/data/iot/string_measurements.csv' DELIMITER ',' CSV HEADER;"
+psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="\copy string_measurements TO '$root/data/iot/string_measurements.csv' DELIMITER ',' CSV HEADER;"
 
 if [ -f "$root/data/iot/boolean_measurements.csv" ]; then
     echo "\nRenaming old boolean_measurements CSV...\n"
     mv $root/data/iot/boolean_measurements.csv $root/data/iot/boolean_measurements.old.csv
 fi
 echo "\nExporting boolean_measurements...\n"
-psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="COPY boolean_measurements TO '$root/data/iot/boolean_measurements.csv' DELIMITER ',' CSV HEADER;"
+psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="\copy boolean_measurements TO '$root/data/iot/boolean_measurements.csv' DELIMITER ',' CSV HEADER;"
 
 
 
@@ -70,14 +70,14 @@ if [ -f "$root/data/utilities/gas.csv" ]; then
     mv $root/data/utilities/gas.csv $root/data/utilities/gas.old.csv
 fi
 echo "\nExporting gas utilities...\n"
-psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$UTILITIES_DB_NAME --command="COPY gas TO '$root/data/utilities/gas.csv' DELIMITER ',' CSV HEADER;"
+psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$UTILITIES_DB_NAME --command="\copy gas TO '$root/data/utilities/gas.csv' DELIMITER ',' CSV HEADER;"
 
 if [ -f "$root/data/utilities/electric.csv" ]; then
     echo "\nRenaming old electric utilities CSV...\n"
     mv $root/data/utilities/electric.csv $root/data/utilities/electric.old.csv
 fi
 echo "\nExporting electric utilities...\n"
-psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$UTILITIES_DB_NAME --command="COPY electric TO '$root/data/utilities/electric.csv' DELIMITER ',' CSV HEADER;"
+psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$UTILITIES_DB_NAME --command="\copy electric TO '$root/data/utilities/electric.csv' DELIMITER ',' CSV HEADER;"
 
 
 
@@ -88,19 +88,19 @@ if [ -f "$root/data/weather/current.csv" ]; then
     mv $root/data/weather/current.csv $root/data/weather/current.old.csv
 fi
 echo "\nExporting current weather...\n"
-psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$WEATHER_DB_NAME --command="COPY current TO '$root/data/weather/current.csv' DELIMITER ',' CSV HEADER;"
+psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$WEATHER_DB_NAME --command="\copy current TO '$root/data/weather/current.csv' DELIMITER ',' CSV HEADER;"
 
 if [ -f "$root/data/weather/hourly.csv" ]; then
     echo "\nRenaming old hourly weather CSV...\n"
     mv $root/data/weather/hourly.csv $root/data/weather/hourly.old.csv
 fi
 echo "\nExporting hourly weather...\n"
-psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$WEATHER_DB_NAME --command="COPY hourly TO '$root/data/weather/hourly.csv' DELIMITER ',' CSV HEADER;"
+psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$WEATHER_DB_NAME --command="\copy hourly TO '$root/data/weather/hourly.csv' DELIMITER ',' CSV HEADER;"
 
 if [ -f "$root/data/weather/daily.csv" ]; then
     echo "\nRenaming old daily weather CSV...\n"
     mv $root/data/weather/daily.csv $root/data/weather/daily.old.csv
 fi
 echo "\nExporting daily weather...\n"
-psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$WEATHER_DB_NAME --command="COPY daily TO '$root/data/weather/daily.csv' DELIMITER ',' CSV HEADER;"
+psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$WEATHER_DB_NAME --command="\copy daily TO '$root/data/weather/daily.csv' DELIMITER ',' CSV HEADER;"
 export PGPASSWORD=
--- services/postgres/import_data.sh
+++ services/postgres/import_data.sh
@@ -8,56 +8,56 @@ export PGPASSWORD=$POSTGRES_PASS
 if [ -f "$root/data/iot/devices.csv" ]; 
 then
     echo "\nImporting iot devices data...\n"
-    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="COPY devices FROM '$root/data/iot/devices.csv' DELIMITER ',' CSV HEADER"
+    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="\copy devices FROM '$root/data/iot/devices.csv' DELIMITER ',' CSV HEADER"
 else
     echo "\nFailed to import iot devices data, $root/data/iot/devices.csv not found...\n"
 fi
 if [ -f "$root/data/iot/locations.csv" ]; 
 then
     echo "\nImporting iot locations data...\n"
-    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="COPY locations FROM '$root/data/iot/locations.csv' DELIMITER ',' CSV HEADER"
+    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="\copy locations FROM '$root/data/iot/locations.csv' DELIMITER ',' CSV HEADER"
 else
     echo "\nFailed to import iot locations data, $root/data/iot/locations.csv not found...\n"
 fi
 if [ -f "$root/data/iot/metrics.csv" ]; 
 then
     echo "\nImporting iot metrics data...\n"
-    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="COPY metrics FROM '$root/data/iot/metrics.csv' DELIMITER ',' CSV HEADER"
+    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="\copy metrics FROM '$root/data/iot/metrics.csv' DELIMITER ',' CSV HEADER"
 else
     echo "\nFailed to import iot metrics data, $root/data/iot/metrics.csv not found...\n"
 fi
 if [ -f "$root/data/iot/measurements.csv" ]; 
 then
     echo "\nImporting iot measurements data...\n"
-    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="COPY measurements FROM '$root/data/iot/measurements.csv' DELIMITER ',' CSV HEADER"
+    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="\copy measurements FROM '$root/data/iot/measurements.csv' DELIMITER ',' CSV HEADER"
 else
     echo "\nFailed to import iot measurements data, $root/data/iot/measurements.csv not found...\n"
 fi
 if [ -f "$root/data/iot/float_measurements.csv" ]; 
 then
     echo "\nImporting iot float_measurements data...\n"
-    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="COPY float_measurements FROM '$root/data/iot/float_measurements.csv' DELIMITER ',' CSV HEADER"
+    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="\copy float_measurements FROM '$root/data/iot/float_measurements.csv' DELIMITER ',' CSV HEADER"
 else
     echo "\nFailed to import iot float_measurements data, $root/data/iot/float_measurements.csv not found...\n"
 fi
 if [ -f "$root/data/iot/integer_measurements.csv" ]; 
 then
     echo "\nImporting iot integer_measurements data...\n"
-    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="COPY integer_measurements FROM '$root/data/iot/integer_measurements.csv' DELIMITER ',' CSV HEADER"
+    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="\copy integer_measurements FROM '$root/data/iot/integer_measurements.csv' DELIMITER ',' CSV HEADER"
 else
     echo "\nFailed to import iot integer_measurements data, $root/data/iot/integer_measurements.csv not found...\n"
 fi
 if [ -f "$root/data/iot/string_measurements.csv" ]; 
 then
     echo "\nImporting iot string_measurements data...\n"
-    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="COPY string_measurements FROM '$root/data/iot/string_measurements.csv' DELIMITER ',' CSV HEADER"
+    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="\copy string_measurements FROM '$root/data/iot/string_measurements.csv' DELIMITER ',' CSV HEADER"
 else
     echo "\nFailed to import iot string_measurements data, $root/data/iot/string_measurements.csv not found...\n"
 fi
 if [ -f "$root/data/iot/boolean_measurements.csv" ]; 
 then
     echo "\nImporting iot boolean_measurements data...\n"
-    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="COPY boolean_measurements FROM '$root/data/iot/boolean_measurements.csv' DELIMITER ',' CSV HEADER"
+    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$IOT_DB_NAME --command="\copy boolean_measurements FROM '$root/data/iot/boolean_measurements.csv' DELIMITER ',' CSV HEADER"
 else
     echo "\nFailed to import iot boolean_measurements data, $root/data/iot/boolean_measurements.csv not found...\n"
 fi
@@ -67,14 +67,14 @@ fi
 if [ -f "$root/data/utilities/gas.csv" ]; 
 then
     echo "\nImporting gas utilities data...\n"
-    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$UTILITIES_DB_NAME --command="COPY gas FROM '$root/data/utilities/gas.csv' DELIMITER ',' CSV HEADER"
+    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$UTILITIES_DB_NAME --command="\copy gas FROM '$root/data/utilities/gas.csv' DELIMITER ',' CSV HEADER"
 else
     echo "\nFailed to import gas utilities data, $root/data/utilities/gas.csv not found...\n"
 fi
 if [ -f "$root/data/utilities/electric.csv" ]; 
 then
     echo "\nImporting electric utilities data...\n"
-    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$UTILITIES_DB_NAME --command="COPY electric FROM '$root/data/utilities/electric.csv' DELIMITER ',' CSV HEADER"
+    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$UTILITIES_DB_NAME --command="\copy electric FROM '$root/data/utilities/electric.csv' DELIMITER ',' CSV HEADER"
 else
     echo "\nFailed to import electric utilities data, $root/data/utilities/electric.csv not found...\n"
 fi
@@ -84,21 +84,21 @@ fi
 if [ -f "$root/data/weather/current.csv" ]; 
 then
     echo "\nImporting current weather data...\n"
-    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$WEATHER_DB_NAME --command="COPY current FROM '$root/data/weather/current.csv' DELIMITER ',' CSV HEADER"
+    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$WEATHER_DB_NAME --command="\copy current FROM '$root/data/weather/current.csv' DELIMITER ',' CSV HEADER"
 else
     echo "\nFailed to import current weather data, $root/data/weather/current.csv not found...\n"
 fi
 if [ -f "$root/data/weather/hourly.csv" ]; 
 then
     echo "\nImporting hourly weather data...\n"
-    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$WEATHER_DB_NAME --command="COPY hourly FROM '$root/data/weather/hourly.csv' DELIMITER ',' CSV HEADER"
+    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$WEATHER_DB_NAME --command="\copy hourly FROM '$root/data/weather/hourly.csv' DELIMITER ',' CSV HEADER"
 else
     echo "\nFailed to import hourly weather data, $root/data/weather/hourly.csv not found...\n"
 fi
 if [ -f "$root/data/weather/daily.csv" ]; 
 then
     echo "\nImporting daily weather data...\n"
-    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$WEATHER_DB_NAME --command="COPY daily FROM '$root/data/weather/daily.csv' DELIMITER ',' CSV HEADER"
+    psql --host=$DB_HOST --port=$DB_PORT --username=$POSTGRES_USER --db=$WEATHER_DB_NAME --command="\copy daily FROM '$root/data/weather/daily.csv' DELIMITER ',' CSV HEADER"
 else
     echo "\nFailed to import daily weather data, $root/data/weather/daily.csv not found...\n"
 fi
